0563e1d196a1f2195065e12c99245845b0874ee0
watchtable <iamgood147@gmail.com>
2021-11-05 22:40:41 +0900

    Initial commit

---
+++ src/main/java/com/ds/watchtable/WatchtableApplication.java
@@ -0,0 +1,13 @@
+package com.ds.watchtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchtableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/watchtable/WatchtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.watchtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchtableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
