e85b4e927d2637dbdbff5006491a8eba5509a874
yoonjeong <dorothyyyj@naver.com>
2021-11-08 15:46:49 +0900

    11.08 15:46 dorothy update

--- src/main/java/com/ds/watchtable/repository/StoreRepository.java
+++ src/main/java/com/ds/watchtable/repository/StoreRepository.java
@@ -2,6 +2,11 @@ package com.ds.watchtable.repository;
 
 import com.ds.watchtable.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface StoreRepository extends JpaRepository <Store, Integer>{
+import java.util.List;
+
+public interface StoreRepository extends JpaRepository <Store, Integer> {
+
 }
+
