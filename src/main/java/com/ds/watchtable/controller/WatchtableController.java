ce585e45210b2cb8d7afb80da6f50ccdb43ace5b
Ginaben <iamgood147@gmail.com>
2021-11-09 19:07:24 +0900

    11.09 19:06 benny is bbac dae ga ri

--- src/main/java/com/ds/watchtable/controller/WatchtableController.java
+++ src/main/java/com/ds/watchtable/controller/WatchtableController.java
@@ -15,8 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping({"/watchtable","/join","/member","/pos","/review","/admin",})
 public class WatchtableController {
     @GetMapping({"/admin","/approve","/approvefiles","/checkpw","/join","/joinselect",
-            "/login","/manage","/manageinfocorrect","/managemyinfo","/managerjoin","/myinfo",
-    "/myinfocorrect","/myreview","/storereview","/wating1","/wating2","/wating3",
+            "/login","/manage","/manageinfocorrect","/managemyinfo","/managerjoin","/myinfo","/myreview","/storereview","/wating1","/wating2","/wating3",
             "/kitchen" , "/postable", "/posorder","/waiting1","/waiting2","/waiting3","/loginmng",
     "/possetting"})
 
--- src/main/java/com/ds/watchtable/service/MemberServiceImpl.java
+++ src/main/java/com/ds/watchtable/service/MemberServiceImpl.java
@@ -1,12 +1,20 @@
 package com.ds.watchtable.service;
 
 import com.ds.watchtable.dto.MemberDTO;
+import com.ds.watchtable.dto.PosDTO;
 import com.ds.watchtable.dto.StoreDTO;
 import com.ds.watchtable.entity.Member;
+import com.ds.watchtable.entity.Pos;
 import com.ds.watchtable.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.ds.watchtable.entity.QMember.member;
 
 @Service
 @Log4j2
@@ -17,9 +25,22 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public String register(MemberDTO memberDTO) {
         Member member = dtoToEntity(memberDTO);
-        memberRepository.save(member);
+        memberRepository.save(member); //저장, 수정
         return member.getMemberName();
     }
+
+    @Override
+    public MemberDTO getInfo(int memberNum) {
+        Optional<Member> result = memberRepository.findById(memberNum);
+        return result.isPresent()?entityToDTO(result.get()):null;
+    }
+
+//    @Override
+//    public MemberDTO getInfo(int memberNum) {
+//        List<Object[]> result = memberRepository.findAll();
+//
+//        return entityToDTO(member);
+//    }
 
     //    @Override
 //    public User add(User user) throws Exception {
