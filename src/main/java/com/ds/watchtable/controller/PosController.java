0322cb256bfff51145da06c6a5ab433642f4237b
sjjkk1996 <58585992+sjjkk1996@users.noreply.github.com>
2021-11-09 17:51:06 +0900

    11 09 17:51

--- src/main/java/com/ds/watchtable/controller/PosController.java
+++ src/main/java/com/ds/watchtable/controller/PosController.java
@@ -1,17 +1,13 @@
 package com.ds.watchtable.controller;
 
 import com.ds.watchtable.dto.PosDTO;
-import com.ds.watchtable.dto.ReviewDTO;
 import com.ds.watchtable.service.PosService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @Log4j2
@@ -27,8 +23,8 @@ public class PosController {
     }
 
     @GetMapping({"/pos/posorder","/pos/postable"})
-    public ResponseEntity<List<PosDTO>>getpos(@PathVariable("posNum") int posNum){
-        List<PosDTO> posDTOList = posService.get;
+    public String gogo(Model model){
+        model.addAttribute("pos","ooo");
         return "/pos/posorder";
     }
 
