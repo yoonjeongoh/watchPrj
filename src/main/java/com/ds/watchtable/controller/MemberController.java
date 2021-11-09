78f7b424ad9a6d076512fcef05dd26fd8feb624a
watchtable <iamgood147@gmail.com>
2021-11-10 01:32:36 +0900

    11.10 01:32 sab jil

--- src/main/java/com/ds/watchtable/controller/MemberController.java
+++ src/main/java/com/ds/watchtable/controller/MemberController.java
@@ -20,7 +20,39 @@ public class MemberController {
     @Autowired
     private final MemberService memberService;
 
-    //
+    @RequestMapping("/member/myinfo")
+    public void registerPost(MemberDTO memberDTO) {
+        log.info("register...........");
+        memberService.register(memberDTO);
+//    attributes.addAttribute("username",userDTO.getUsername());
+//    attributes.addAttribute("userid",userDTO.getUserid());
+    }
+
+//    @GetMapping("/member/myinfocorrect") //get-불러오기
+//    private void getInfo(Long memberNum, Model model) {
+//        MemberDTO memberDTO = memberService.get(memberNum);
+//        model.addAttribute("memberDTO0", memberDTO);
+//    }
+}
+
+
+
+//  ----삽질의 흔적-------------------------------------------
+
+//    //db넘기는거 확인-ok
+//    @GetMapping("/member/myinfocorrect")
+//    public String getInfo(Model model){
+//        model.addAttribute("data","tlqkf");
+//        return "/member/myinfocorrect";
+//    }
+
+
+    //    @GetMapping("/member/myinfocorrect")
+//    public void getInfo(int memberNum, @ModelAttribute("dto") Model model) {
+//        MemberDTO memberDTO = memberService.getInfo(memberNum);
+//        model.addAttribute("dto", memberDTO);
+//    }
+
 //    @PostMapping("{usernum}")
 //    public ResponseEntity<Integer> addUser(@RequestBody UserDTO userDTO){
 //        log.info("----------add user--");
@@ -35,25 +67,4 @@ public class MemberController {
 //        return "redirect:/myinfo";
 //    }
 
-    @RequestMapping("/member/myinfo")
-    public void registerPost(MemberDTO memberDTO) {
-        log.info("register...........");
-        memberService.register(memberDTO);
-//    attributes.addAttribute("username",userDTO.getUsername());
-//    attributes.addAttribute("userid",userDTO.getUserid());
-    }
 
-//    @GetMapping("/member/myinfocorrect")
-//    public void getInfo(int memberNum, @ModelAttribute("dto") Model model) {
-//        MemberDTO memberDTO = memberService.getInfo(memberNum);
-//        model.addAttribute("dto", memberDTO);
-//    }
-
-    @RequestMapping("/member/myinfocorrect")
-    public String getInfo( Model model){
-        model.addAttribute("data","tlqkf");
-        return "/member/myinfocorrect";
-    }
-
-
-}
--- src/main/java/com/ds/watchtable/repository/MemberRepository.java
+++ src/main/java/com/ds/watchtable/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
 }
