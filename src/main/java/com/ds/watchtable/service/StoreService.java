42efbf15eedbab3462fc9bbf767d2387d20ae3b6
watchtable <iamgood147@gmail.com>
2021-11-14 19:50:55 +0900

    11.14 19:50 benny.....bbac dae ga ri

--- src/main/java/com/ds/watchtable/service/StoreService.java
+++ src/main/java/com/ds/watchtable/service/StoreService.java
@@ -19,7 +19,6 @@ public interface StoreService {
         default Map<String, Object> dtoToEntity(StoreDTO storeDTO) {
         Map<String, Object> entityMap = new HashMap<>();
         Store store = Store.builder()
-/*
                 .member(Member.builder().memberNum(storeDTO.getMemberNum())
                         .memberName(storeDTO.getMemberName())
                         .memberNickname(storeDTO.getMemberNickname())
@@ -28,7 +27,6 @@ public interface StoreService {
                         .memberPw(storeDTO.getMemberPw())
                         .memberMobile(storeDTO.getMemberMobile())
                         .build())
-*/
                 .storeNum(storeDTO.getStoreNum())
                 .storeName(storeDTO.getStoreName())
                 .storeAds(storeDTO.getStoreAds())
