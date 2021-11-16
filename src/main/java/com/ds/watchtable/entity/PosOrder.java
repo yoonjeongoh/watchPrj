178c59ac0bcf3f61dd3da3fead3b9d1009e07a5d
sjjkk1996 <58585992+sjjkk1996@users.noreply.github.com>
2021-11-16 17:52:51 +0900

    11 16 17:52 um...

--- src/main/java/com/ds/watchtable/entity/PosOrder.java
+++ src/main/java/com/ds/watchtable/entity/PosOrder.java
@@ -16,7 +16,23 @@ public class PosOrder extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderNum;
 
-    private String orderList;
+    private String order1;
+    private String order2;
+    private String order3;
+    private String order4;
+
+    private String orderNum1;
+    private String orderNum2;
+    private String orderNum3;
+    private String orderNum4;
+
+    private String drink1;
+    private String drink2;
+
+    private String drinkNum1;
+    private String drinkNum2;
+
+
 
 
 }
