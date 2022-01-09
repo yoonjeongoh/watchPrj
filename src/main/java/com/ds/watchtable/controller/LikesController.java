83359b716712d4ee2e1b58a458ccf83712fe9b44
watchtable <iamgood147@gmail.com>
2022-01-10 01:16:41 +0900

    01.10 01:16 좋아요 할거야,,

--- src/main/java/com/ds/watchtable/controller/LikesController.java
+++ src/main/java/com/ds/watchtable/controller/LikesController.java
@@ -1,8 +1,14 @@
 package com.ds.watchtable.controller;
 
+import com.ds.watchtable.dto.LikesDTO;
+import com.ds.watchtable.service.LikesService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping
 public class LikesController {
+    @Autowired
+    final private LikesService likesService;
+
+    //좋아요 추가
+    @RequestMapping("/addLikes/{storeNum}")
+    public ResponseEntity<Long> addLikes(@RequestBody LikesDTO likesDTO) {
+        Long likesNum = likesService.registerLikes(likesDTO);
+        return new ResponseEntity<>(likesNum, HttpStatus.OK);
+    }
+
+
 }
