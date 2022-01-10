860b4792701c854aec5ca3d1da6413de8d45cc3e
watchtable <iamgood147@gmail.com>
2022-01-11 00:24:29 +0900

    01.11 00:24

---
+++ src/main/resources/static/js/callWaiting.js
@@ -0,0 +1,5 @@
+//managemyinfo에서 쓰는 스크립트
+$(document).ready(function(e){
+    var contextPath = $('#contextPathHolder').attr('data-contextPath') ? $('#contextPathHolder').attr('data-contextPath') : '';
+
+});
