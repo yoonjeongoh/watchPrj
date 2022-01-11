f22c7706938a3943e878c519b1be8313635b2ce6
watchtable <iamgood147@gmail.com>
2022-01-12 01:15:23 +0900

    01.12 01:14 매니저 웨이팅 모달 준비

--- src/main/resources/static/js/callWaiting.js
+++ src/main/resources/static/js/callWaiting.js
@@ -2,4 +2,15 @@
 $(document).ready(function(e){
     var contextPath = $('#contextPathHolder').attr('data-contextPath') ? $('#contextPathHolder').attr('data-contextPath') : '';
 
+//모달 띄우기
+    $(".waiting-table").on("click",".waiting-box", function(){
+    console.log("click_modal");
+        $('.waitingModal').modal('show');
+    });
+
+    //대기 모달 닫기
+    $(".waiting-close21").click(function(){
+        $('.waitingModal').modal('hide')
+    });
+
 });
